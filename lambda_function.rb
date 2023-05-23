752f9ef9c1b37fb1d0e51988e63390a304b6b3cb
miran muthanna <miranmuthannak.s@gmail.com>
2023-05-23 12:50:36 +0000

    Update lambda_function.rb

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,31 +1,6 @@
 require 'json'
-require 'simpletable'
-
-class TestObject
-  attr_reader :a, :b, :c
-  def initialize(a,b,c)
-    @a = a
-    @b = b
-    @c = c
-  end
-end
 
 def lambda_handler(event:, context:)
     # TODO implement
-
-    objs = []
-    objs << TestObject.new("A1", "B1", "C1")
-    objs << TestObject.new("A2", "B2", "C2")
-    objs << TestObject.new("A3", "B3", "C3")
-    objs << TestObject.new("A4", "B4", "C4")
-
-    titles = ["ColA", "ColB", "ColC"]
-    methods = [:a, :b, :c]
-
-    t = SimpleTable.new
-
-    tstr = t.from_objects(objs, titles, methods).text
-
-    { statusCode: 200, body: tstr }
+    { statusCode: 200, body: JSON.generate('Hello from Miran!') }
 end
-
